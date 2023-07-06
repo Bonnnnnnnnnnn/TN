1a569256bbedaad1dec8f8bc3265e5465bd0dae0
Tu Kiet <tukietpro123@gmail.com>
2023-07-06 22:32:51 +0700

    Revert "Merge remote-tracking branch 'origin/hai'"
    This reverts commit 4e41e7f1822fc2e815de928d36725e9ad0023e03, reversing
    changes made to 47293ef2198f5607a0a624b22f744db46a931a65.

--- src/main/java/com/poly/service/FavouriteService.java
+++ src/main/java/com/poly/service/FavouriteService.java
@@ -10,19 +10,17 @@ import com.poly.model.Product;
 
 public interface FavouriteService {
 	
-	Favourite findByUsernameAndProductId(String username, String id);
+	Favourite findByUsernameAndProductId(String username, String productId);
 	
 	void delete(Favourite favourite);
 
 	Favourite create(Account account, Product product);
 	
-	Favourite updateLikeOrUnlike(Account account, String id);
+	Favourite updateLikeOrUnlike(Account account, String productId);
 
 	// Bởi vì câu truy vấn trả về một số lượng cột không xác định trước, nên là sử dụng kiểu dữ liệu Object[] để chứa kết quả trả về.
 	List<Object[]> getTotalLikesOfProduct();
 	
 	// Hiển thị thông tin những người đã like sp theo mã sản phẩm
-	List<Object[]> getUserInfoWithProductIsLikedByUsers(String id);
-
-
+	List<Object[]> getUserInfoWithProductIsLikedByUsers(String productId);
 }
