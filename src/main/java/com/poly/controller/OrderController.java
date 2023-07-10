569ba73e8558ca82990b1357c5e812098347b39f
LongNgoo <107351915+LongNgoo@users.noreply.github.com>
2023-07-10 12:20:44 +0700

    Long

--- src/main/java/com/poly/controller/OrderController.java
+++ src/main/java/com/poly/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.poly.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +22,16 @@ public class OrderController {
 	
 	@Autowired
 	CategoryService categoryService;
+	
+	@RequestMapping("/order/list")
+	public String list(Model model, HttpServletRequest request) {
+		List<Category> categories = categoryService.findAll();
+        model.addAttribute("categories", categories);
+        
+		String username = request.getRemoteUser();
+		model.addAttribute("orders", orderService.findByUsername(username));
+		return "user/order/list";
+	}
 	
 	@RequestMapping("/order/detail/{id}")
 	public String detail(@PathVariable("id") long id, Model model) {
