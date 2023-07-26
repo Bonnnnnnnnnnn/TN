1e0fe5a29e12c684064ba83e1b3cebd55ff96926
Tu Kiet <tukietpro123@gmail.com>
2023-07-26 10:07:25 +0700

    cc

--- src/main/resources/static/assets/admin/js/webapp.js
+++ src/main/resources/static/assets/admin/js/webapp.js
@@ -29,9 +29,6 @@ app.config(function($routeProvider){
 	.when("/cancelled", {
 		templateUrl: "/assets/admin/layout/cancelled.html"
 	})
-	.when("/dashboard", {
-		templateUrl: "/assets/admin/layout/dashboard.html"
-	})
 	.otherwise({
 		redirectTo: ""
 	});
